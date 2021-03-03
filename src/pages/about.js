2f09fed1ab564ab14d5b1c0ef72408fa335e2b8f
Kurt Trowbridge <kurt@gravityworksdesign.com>
2021-03-03 13:13:08 -0500

    Reduced index page content; added About page

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+
+// markup
+const myComponent = () => {
+  return (
+    <main>
+      <title>About</title>
+      <h1>
+        About my site
+      </h1>
+      <p>It's beautiful.</p>
+    </main>
+  )
+}
+
+export default myComponent
